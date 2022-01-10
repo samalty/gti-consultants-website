b0ec9cc77d164f47f0637db1daf270c126403fa2
Sam Alty <sam_alty@outlook.com>
2022-01-10 18:54:52 +0000

    Updated fav and footer styling.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,17 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './styles/Footer.scss';
 import logo from './media/logo-small.png';
-import icon from './media/icon.png';
 import NavbarData from './data/NavbarData';
 
 class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                <div className="logo-container">
-                    <img src={logo} alt="GTIC" className="footer-logo logo"></img>
-                    <img src={icon} alt="GTIC" className="footer-logo icon"></img>
-                </div>
                 <div className="links">
                     <ul>
                         {NavbarData.filter(NavbarData => NavbarData.title).map((item, index) => {
@@ -24,6 +19,7 @@ class Footer extends Component {
                         })}
                     </ul>
                 </div>
+                <img src={logo} alt="GTIC" className="footer-logo"></img>
             </div>
         )
     }
